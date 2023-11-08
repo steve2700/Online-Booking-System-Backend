2087599d40e474ddff15d12ec4b4ac3879baae3f
steve2700 <stevnyaru@github.com>
2023-11-08 21:41:57 +0200

    improved our user and admin models

--- backend/models/admin.py
+++ backend/models/admin.py
@@ -1,24 +1,56 @@
 #!/usr/bin/env python3
-'''
-Admin entity Module
-'''
-from app import db
-from event import Event
+"""Admin entity Module"""
+
+from backend import db
+import bcrypt
 
 
 class Admin(db.Model):
-    '''Amin entity class'''
-    id = db.Column(db.Integer, primary_key=True)
+    """Admin entity class"""
+
+    __tablename__ = "admins"
+
+    # Override the id attribute to have a different type
+    id = db.Column(
+        db.String(60), primary_key=True, unique=True, nullable=False
+    )
+
+    # Define columns for the Admin table
     username = db.Column(db.String(50), unique=True, nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
-    password = db.Column(db.String(100), nullable=False)
+    _password = db.Column(db.String(255), nullable=False)
     company = db.Column(db.String(100), nullable=False)
-    position = dc.Column(db.String(100), nullable=False)
+    position = db.Column(db.String(100), nullable=False)
     first_name = db.Column(db.String(50), nullable=False)
     last_name = db.Column(db.String(50), nullable=False)
-    events = db.ralationship('Event', backref='admin', lazy=True)
-    
+
+    # Relationship with Event model
+    event = db.relationship(
+        "Event", backref="admin", cascade="all, delete-orphan", lazy=True
+    )
+
     @property
     def full_name(self):
-        '''Returns the admin full name'''
-        return f'{self.first_name} {self.last_name}'
+        """Returns the admin full name"""
+        return f"{self.first_name} {self.last_name}"
+
+    @property
+    def password(self):
+        """Returns the hashed password"""
+        return self._password
+
+    @password.setter
+    def password(self, password):
+        """Sets the hashed password"""
+        self._password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+    def check_password(self, password):
+        """Verifies if the provided password matches the hashed password"""
+        return bcrypt.checkpw(password.encode('utf-8'), self._password.encode('utf-8'))
+
+    def __repr__(self):
+        """Return a string representation of the Admin object"""
+        return "Id: {}, Username: {}, Name: {}, Email: {}".format(
+            self.id, self.username, self.full_name, self.email
+        )
+
--- backend/models/user.py
+++ backend/models/user.py
@@ -1,24 +1,57 @@
 #!/usr/bin/env python3
-'''
-User entity module
-'''
-from app import db
+"""Template for the User Class"""
+
+from backend import db
+from backend.models.base_model import BaseModel
 
 
-class User(db.Model):
-    '''User model class'''
-    id = db.Column(db.Integer, primary_key=True)
+class Users(BaseModel):
+    """User model"""
+
+    __tablename__ = "users"
+
+    # Override the id attribute to have a different type
+    id = db.Column(
+        db.String(60), primary_key=True, unique=True, nullable=False
+    )
+
+    # Define columns for the Users table
     username = db.Column(db.String(50), unique=True, nullable=False)
-    email = db.Column(db.String(100), unique=True, nullable=False)
-    password = db.Column(db.String(100), nullable=False)
-    first_name = db.Column(db.String(50), nullable=False)
-    last_name = db.Column(db.String(50), nullable=False)
-    events = db.relationship('Event', backref='user', lazy=True )
-    
-    @property
-    def full_name(self):
-        '''Retuns the full name dictionary'''
+    name = db.Column(db.String(120), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    access_token = db.Column(db.String(120), nullable=False)
+    refresh_token = db.Column(db.String(120), nullable=False)
+    avatar = db.Column(db.String(255), nullable=False)
+
+    def __init__(self, id, username, name, email, avatar):
+        """_summary_
+
+        Args:
+            id (_type_): _description
+            username (_type_): The user's username.
+            name (_type_): The user's name.
+            email (_type_): The user's email address.
+            avatar (_type_): The URL of the user's avatar image.
+        """
+        self.id = id
+        self.username = username
+        self.name = name
+        self.email = email
+        self.avatar = avatar
+
+    def __repr__(self):
+        """Return a string representation of the User object"""
+        return "Id: {}, Username: {}, Name: {}, Email: {}".format(
+            self.id, self.username, self.name, self.email
+        )
+
+    # Override the format method to return event attributes as a dictionary
+    def format(self):
+        """Return a dictionary representation of the User object"""
         return {
-            'firstname': self.first_name,
-            'lastname': self.last_name
+            "username": self.username,
+            "name": self.name,
+            "email": self.email,
+            "avatar": self.avatar,
         }
+
